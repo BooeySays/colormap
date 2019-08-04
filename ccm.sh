f40f16fa3b62b43f06ad3da8a8afc9106a594d91
BooeySays <dbuihien@my.bcit.ca>
2019-08-04 16:53:06 -0700

    added blocks instead of space

--- ccm.sh
+++ ccm.sh
@@ -7,7 +7,7 @@ while [ "$XXX" -lt '232' ]; do
     while [ "$LINE" -gt '0' ]; do
         for i in {"$((XXX + 1))","$((XXX + 2))","$((XXX + 3))","$((XXX + 4))","$((XXX + 5))","$((XXX + 6))"}; do
             printf '%03d' " $i"
-            echo -en "[m:\033[48;5;"$i"m   \033[m [01m"
+            echo -en "[m:\033[38;5;"$i"m▆▆▆\033[m [01m"
         done
         echo
         LINE="$((LINE-1))"
