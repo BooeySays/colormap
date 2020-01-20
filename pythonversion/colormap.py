acb28e7b2c38f2e9e4e9e405ad67580826447f40
BooeySays <dbuihien@my.bcit.ca>
2020-01-19 22:04:40 -0800

    merged all funcs into one

--- pythonversion/colormap.py
+++ pythonversion/colormap.py
@@ -4,11 +4,11 @@ import sys, os
 
 initcount = 0
 
-def cmapa():
+def cmap():
 	print('[00;01mBasic Colors: [m')
 	initcount = 0
 	count = 7
-	print(' ' * 18 + '[00;01m[Basic: 0-7][m')
+#	print(' ' * 18 + '[00;01m[Basic: 0-7][m')
 	while count >= 0:
 #		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ': [38;5;' + str(initcount) + 'm▆▆▆▆[m ',end='')
 		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ':[38;5;' + str(initcount) + 'm▆▆[m ',end='')
@@ -22,9 +22,8 @@ def cmapa():
 		initcount += 1
 		count -= 1
 	print('')
-	print(' ' * 18 + '[00;01m[Bold: 8-15][m\n')
-
-def cmapb():
+#	print(' ' * 18 + '[00;01m[Bold: 8-15][m\n')
+	print('')
 	print('[00;01mAdvanced colors: [m')
 	initcount = 16
 	while initcount < 232:
@@ -49,10 +48,5 @@ def cmapb():
 			count -= 1
 		print('')
 
-#def cmapc():
-#	initcount = 232
 
-
-cmapa()
-cmapb()
-#cmapc()
+cmap()
