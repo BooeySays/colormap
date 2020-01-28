69b42e2e32632911eaa2806037c5e57f8c338a67
BooeySays <dbuihien@my.bcit.ca>
2020-01-28 17:44:40 -0500

    tweaked script to show colored background instead of colored blocks because some terminals cant show the blocks

---
+++ pythonversion/colormapb.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys, os
+
+initcount = 0
+version = '0.5'
+
+def cmap():
+	print('[00;01mBasic Colors: [m')
+	initcount = 0
+	count = 7
+#	print(' ' * 18 + '[00;01m[Basic: 0-7][m')
+	while count >= 0:
+#		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ': [38;5;' + str(initcount) + 'm▆▆▆▆[m ',end='')
+		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ':[48;5;' + str(initcount) + 'm  [m ',end='')
+		initcount += 1
+		count -= 1
+	print('')
+	count = 7
+	while count >= 0:
+#		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ': [38;5;' + str(initcount) + 'm▆▆▆▆[m ',end='')
+		print('[m  ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ':[48;5;' + str(initcount) + 'm  [m ',end='')
+		initcount += 1
+		count -= 1
+	print('')
+#	print(' ' * 18 + '[00;01m[Bold: 8-15][m\n')
+	print('')
+	print('[00;01mAdvanced colors: [m')
+	initcount = 16
+	while initcount < 232:
+		countb = 6
+		while countb > 0:
+			count = 6
+			while count > 0:
+#				print('[m   ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ': [38;5;' + str(initcount) + 'm▆▆▆▆[m ',end='')
+				print('[m   ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ':[48;5;' + str(initcount) + 'm   [m ',end='')
+				initcount += 1
+				count -= 1
+			countb -= 1
+			print('')
+		print('')
+	print('[00;01mShades of grey: [m')
+	while initcount < 256:
+		count = 6
+		while count > 0:
+#			print('[m   ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ': [38;5;' + str(initcount) + 'm▆▆▆▆[m ',end='')
+			print('[m   ' + '[' + str(len(str(initcount))) + 'D' + str(initcount) + ':[48;5;' + str(initcount) + 'm   [m ',end='')
+			initcount += 1
+			count -= 1
+		print('')
+
+
+cmap()
